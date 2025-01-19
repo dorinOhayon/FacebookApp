71ad9a80fb7c3d79bab083cad6051f1e45794bd5
Dorin Ohayon <76781235+dorinOhayon@users.noreply.github.com>
2025-01-19 13:00:04 +0200

    Update Program.cs

--- FacebookWinFormsApp/Program.cs
+++ FacebookWinFormsApp/Program.cs
@@ -2,10 +2,6 @@
 using System.Windows.Forms;
 using FacebookWrapper;
 
-// $G$ RUL-004 (-20) Wrong word name format
-// $G$ THE-001 (-14) your grade on diagrams document - 86. please see comments inside the document. 40% of your grade).
-// $G$ CSS-999 (-10) StyleCop errors
-
 namespace FacebookWinFormsApp
 {
     static class Program
